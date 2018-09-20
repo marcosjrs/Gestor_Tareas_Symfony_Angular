8104db2d07acbc36091a0d2417155592a5013a8f
marcosjrs <marcosjrs80@gmail.com>
2018-09-20 22:59:55 +0200

    Add una action de prueba de acceso a datos.

--- symfony/src/AppBundle/Controller/DefaultController.php
+++ symfony/src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use BackendBundle\Entity\User;
 
 class DefaultController extends Controller
 {
@@ -17,5 +18,33 @@ class DefaultController extends Controller
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
+    }
+
+    /**
+     * Prueba de acceso a la "BBDD", añadiendo un usuario e imprimiendo el numero de usuarios.
+     * Accesible mediante una url similar a http://localhost/gestor-tareas-symfony-angular/symfony/web/app_dev.php/testadduser
+     * @Route("/testadduser", name="testadduser")
+     */
+    public function testadduserAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $repoUser = $em->getRepository("BackendBundle:User");
+
+        $user = new User();
+        $user->setName("Marcos3");
+        $user->setSurname("RS");
+        $user->setEmail("marcos3@email.com");
+        $user->setPassword("1234");
+        $user->setCreatedAt(new \DateTime("now"));
+
+        $em->persist($user);
+        $em->flush();
+
+        $users = $repoUser->findAll();
+        $count = count($users);
+
+        echo "Numero de usuarios: $count ";
+
+        die();//para que se interrumpa, y no requiera una "vista"
     }
 }
