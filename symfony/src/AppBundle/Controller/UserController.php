ded2f5ce0bbe88de257928cfcf5bf6fab8068bf6
marcosjrs <marcosjrs80@gmail.com>
2018-09-22 10:21:45 +0200

    Implementación creacion de nuevo usuario en UserController

--- symfony/src/AppBundle/Controller/UserController.php
+++ symfony/src/AppBundle/Controller/UserController.php
@@ -21,8 +21,55 @@ class UserController extends Controller
     }
 
     public function newAction(Request $request){
-        echo "[UserController] new";
-        die();
+        //Recogida de parametro json que deberá contener todos los datos.
+        $json = $request->get('json',null);
+        $helpers = $this->get(Helpers::class);
+        if(is_null($json)){
+            $data = array('status'=>'error', 'code'=>400, 'data'=>'Faltan parámetro "json"');
+        }else{
+            //recogemos los atributos del parametro 'json'
+            $params = json_decode($json);
+            $email = (isset($params->email)) ? $params->email : null;
+            $password = (isset($params->password)) ? $params->password : null;
+            $name = (isset($params->name)) ? $params->name : null;
+            $surname = (isset($params->surname)) ? $params->surname : null;
+            $createdAt = new \Datetime("now");
+            $role = 'user';
+            
+
+            //verificamos formato email            
+            $validatorEmail = new Assert\Email();
+            $validatorEmail->message = "Formato de email no válido";
+            $errorsValidations = $this->get("validator")->validate($email, $validatorEmail);
+
+            $validData = (count($errorsValidations) == 0 
+                            && $email != null && $password != null  
+                            && $name != null && $surname != null );
+            if($validData){ 
+                //creamos el usuario               
+                $user = new User();
+                $user->setEmail($email);   
+                $user->setPassword($password);   
+                $user->setName($name);   
+                $user->setSurname($surname);   
+                $user->setCreatedAt($createdAt);   
+                $user->setRole($role);   
+
+                $em = $this->getDoctrine()->getManager();
+                $repoUser = $em->getRepository("BackendBundle:User");
+                $existsUser = $repoUser->findBy(array( "email"=>$email ));
+                if(count($existsUser) == 0){
+                    $em->persist($user);
+                    $em->flush();
+                    $data = array('status'=>'success', 'code'=>200, 
+                            'data'=>'Usuario creado correctamente', 'user'=>$user);
+                }else{
+                    $data = array('status'=>'error', 'code'=>400, 'data'=>'Ese email ya existe');
+                }               
+                
+            }            
+        }
+        return  $this->get(Helpers::class)->jsonParser($data);
     }
 
 }
