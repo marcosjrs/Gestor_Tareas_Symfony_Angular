9b2c40befe04d30b97ed264e8ecc865e01ff2d67
marcosjrs <marcosjrs80@gmail.com>
2018-10-14 12:27:47 +0200

    paginación y loaders de las tareas

--- angular/src/app/components/default.component.ts
+++ angular/src/app/components/default.component.ts
@@ -10,26 +10,60 @@ import { Task } from '../models/task';
   styleUrls: ['../views/default.component.css']
 })
 export class DefaultComponent implements OnInit {
-  public tasks:Array<Task>;
+  public tasks: Array<Task>;
+  //Para paginación
+  public loadingPage;
+  public internalLoading;
+  public actualPage;
+  public pages;
+  public pagePrev;
+  public pageNext;
+  private token;
+  public noLogin;
 
-  constructor(private _route:ActivatedRoute, private _router:Router, private _userService:UserService, private _taskService:TaskService) {    
+  constructor(private _route: ActivatedRoute, private _router: Router, private _userService: UserService, private _taskService: TaskService) {
   }
 
   ngOnInit() {
-    this.getAllTasks();
+    this.token = this._userService.getLocalToken();
+    if(this.token){
+      this.getAllTasks();
+    }else{
+      this.noLogin = true;
+    }
   }
 
-  getAllTasks(){
-    let page = +this._route.snapshot.params.page;    
+  loadPage(actPage){
+    this.actualPage = actPage;
+    this.internalLoading = true;
+    this._taskService.get(this.token, this.actualPage).subscribe(
+      resp => {
+        if (resp.json().status == "success") {
+          const respJson = resp.json();
+          this.tasks =respJson.data;
 
-    this._taskService.get(this._userService.getLocalToken(), page ? page : 1).subscribe(
-      resp=>{
-        if(resp.json().status=="success"){
-          this.tasks = resp.json().data;
+          //Total paginas
+          this.pages = [];
+          for (let i = 0; i <respJson.total_pages; i++) {
+            this.pages.push(i);
+          }
+          //Establecer pagina anterior y pagina siguiente
+          this.pagePrev = this.actualPage > 1 ?this.actualPage - 1 : this.actualPage;
+          this.pageNext = this.actualPage < respJson.total_pages ? this.actualPage + 1 : this.actualPage;
+          //ocultamos loader
+          this.internalLoading = false;
+          this.loadingPage = false;
         }
       },
-      err=>console.log(err)
+      err => console.log(err)
     );
   }
+
+  getAllTasks() {
+    this.loadingPage = true;
+    let actualPage = +this._route.snapshot.params.page;
+    actualPage = actualPage ? actualPage : 1;
+    this.loadPage(actualPage);
+  } 
 
 }
